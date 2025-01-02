def78e3ab30f89f2fd2ab63dcb2cad782c1d246f
dybbuk.icp <87137460+giremu@users.noreply.github.com>
2025-01-02 09:08:44 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript for handling the "Buy RockCoin Now" button click
+function redirectToBuyPage() {
+    window.location.href = "https://www.rockcoin-exchange.com"; // Replace with actual exchange URL
+}
